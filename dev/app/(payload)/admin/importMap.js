99ebc4107da606f401ad8b51007f45b553bcd0be
sneddonisaac <info.isaacsneddon@gmail.com>
2026-05-26 13:14:02 +0100

    feat: initial commit

---
+++ dev/app/(payload)/admin/importMap.js
@@ -0,0 +1,9 @@
+import { BeforeDashboardClient as BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343 } from 'advanced-forms-plugin/client'
+import { BeforeDashboardServer as BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f } from 'advanced-forms-plugin/rsc'
+
+export const importMap = {
+  'advanced-forms-plugin/client#BeforeDashboardClient':
+    BeforeDashboardClient_fc6e7dd366b9e2c8ce77d31252122343,
+  'advanced-forms-plugin/rsc#BeforeDashboardServer':
+    BeforeDashboardServer_c4406fcca100b2553312c5a3d7520a3f,
+}
